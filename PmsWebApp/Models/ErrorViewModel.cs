4c8b838e577196814c5aaf877120c2bddc3fcfc6
Mbonisi-The-Dev <65551515+Mbonisi-The-Dev@users.noreply.github.com>
2020-07-15 01:33:08 +0200

    Add project files.

---
+++ PmsWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PmsWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
